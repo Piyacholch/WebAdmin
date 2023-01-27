c68c29b7c0fe1d1670b48f7104c6659cee9a4188
GlerMain <Piyachol.ch@kkumail.com>
2023-01-27 11:16:35 +0700

    update

--- src/views/update/_id/updateProfile.vue
+++ src/views/update/_id/updateProfile.vue
@@ -36,6 +36,13 @@
           </div>
 
           <div class="form-floating mb-3">
+            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" v-model="photoURL"
+              />
+            <label for="floatingInput">รูปภาพ</label>
+          </div>
+
+
+          <div class="form-floating mb-3">
             <input type="text" class="form-control icon" placeholder="dispassword" v-model="data.displayName" />
             <div class="invalid-feedback">Please choose a username.</div>
             <span class="material-symbols-outlined"> edit </span>
